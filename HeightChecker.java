aec696783d08b2d1004e4ef0ab7f7a69a127b0ac
Anna Chaykovsky <annachaykovsky@gmail.com>
2021-09-14 09:27:10 +0300

    Height Checker

---
+++ HeightChecker.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int heightChecker(int[] heights) {
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+        int differentPositon = 0;
+        for (int i = 0; i < heights.length; i++) {
+
+            if (heights[i] != expected[i])
+                differentPositon++;
+        }
+        return differentPositon;
+    }
+}
+
+
