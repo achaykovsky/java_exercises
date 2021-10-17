9e1326e7edf78a8bd4e3acf1c862563ebb6f5793
Anna Chaykovsky <annachaykovsky@gmail.com>
2021-10-17 11:18:37 +0300

    Final Value of Variable After Performing Operations

---
+++ FinalValueVariableAfterPerformingOperations.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].charAt(1) == '+')
+                result++;
+            else
+                result--;
+        }
+        return result;
+    }
+}
